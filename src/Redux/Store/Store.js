c12ac3a296cb648fd59722a99137c9173593006e
MargarTakvoryan <gtakvoryn@gmail.com>
2024-01-22 21:26:59 +0400

    create Redxu Store and Components file

---
+++ src/Redux/Store/Store.js
@@ -0,0 +1,5 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
+
+
+const Store = createStore(combineReducers(),{},applyMiddleware(thunk))

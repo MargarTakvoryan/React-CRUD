c385377a25813d96eba99d6f308e19388caaa0ce
MargarTakvoryan <gtakvoryn@gmail.com>
2024-01-29 00:14:05 +0400

    Create-Porvider

--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -2,4 +2,12 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
 
 
-const Store = createStore(combineReducers(),{},applyMiddleware(thunk))
+const Store = createStore(
+    combineReducers(
+        {
+
+        }
+    ),
+    applyMiddleware(thunk))
+
+export default Store
